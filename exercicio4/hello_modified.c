93d04e4ed738f8055d4434549306dd1adff2dc15
Pedro Grijó <pedrgrijo@gmail.com>
2016-06-12 22:23:40 -0300

    Update hello_modified.c

---
+++ exercicio4/hello_modified.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PERIPHERAL_BASE_ADDRESS 0x6400000
+
+int main() {
+
+	volatile int *base = (volatile int *)PERIPHERAL_BASE_ADDRESS;
+	*base = 1; 
+
+	printf("Base content: %d\n", *base);
+	return 0;
+}
